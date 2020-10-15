b4926dae02a8ffda488d930b417f833387f3cf59
Adam Jędrzejec <adamjedrzejec@gmail.com>
2020-10-16 01:20:54 +0200

    [add] new class: generating samples

--- main.py
+++ main.py
@@ -3,11 +3,50 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-d = {'x': [1, 2], 'y': [3, 5], 'class': [1, 2]}
+graphData = {'x': [], 'y': [], 'classType': []}
+
+
+class Classification:
+    x = 0.5
+    y = 0.75
+    classType = 1
+
+    df = pd.DataFrame(data={'x': [], 'y': [], 'classType': []})
+
+    def __init__(self, x, y, classType):
+        self.x = x
+        self.y = y
+        self.classType = classType
+
+    def makeNewSample(self, howMany):
+        if howMany <= 0:
+            print('\'howMany\' cannot be smaller or equal to 0')
+
+        if howMany > 0:
+            for i in range(howMany):
+                tempX = np.random.normal(loc=self.x, scale=0.2)
+                tempY = np.random.normal(loc=self.y, scale=0.2)
+
+                self.df = self.df.append(
+                    {'x': tempX, 'y': tempY, 'classType': self.classType}, ignore_index=True)
+
+# def getAllTogether():
+
+
+cl1 = Classification(0.5, 0.75, 1)
+cl1.makeNewSample(30)
+
+cl2 = Classification(3, 3, 2)
+cl2.makeNewSample(30)
+
+d = {'x': [0, 5], 'y': [0, 5], 'classType': [0, 0]}
 df = pd.DataFrame(data=d)
 
-df = df.append({'x': 14, 'y': 14, 'class': 1}, ignore_index=True)
+# df = df.append({'x': 14, 'y': 14, 'class': 1}, ignore_index=True)
 
-sns.scatterplot(data=df, x='x', y='y', hue='class')
+df = df.append(cl1.df)
+df = df.append(cl2.df)
+
+sns.scatterplot(data=df, x='x', y='y', hue='classType')
 
 plt.show()
